ea7068d1e37a1f93778e5bc8d1d431fc319373bb
AlenaGM <alena.guillaume4@gmail.com>
2023-10-07 18:22:40 +0200

    animated sections 1-4

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,199 @@
 /* eslint-disable no-undef */
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("OK");
-  gsap.set(".promo__title", { color: "red" });
+  animate();
 });
+
+function animate() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.set(".about__bg", {
+    yPercent: 20,
+  });
+
+  gsap.set(".about__wrapper", {
+    yPercent: 200,
+  });
+
+  const tlPromo = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top top",
+      end: "bottom+=100%",
+      pin: true,
+      scrub: 1,
+    },
+    onComplete: () => {
+      ScrollTrigger.refresh();
+    },
+  });
+
+  tlPromo
+    .to(".about__bg", {
+      yPercent: 0,
+    })
+    .to(
+      ".about__inner",
+      {
+        width: "100%",
+        height: "100%",
+      },
+      "<"
+    )
+    .to(
+      ".about__decor",
+      {
+        opacity: 0,
+      },
+      "<"
+    )
+    .to(
+      ".about__wrapper",
+      {
+        yPercent: 0,
+      },
+      "<"
+    );
+
+  const tlAim = gsap.timeline({
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".aim",
+      start: "top top",
+      end: "bottom+=120%",
+      pin: true,
+      scrub: 1,
+    },
+  });
+
+  tlAim
+    .to(
+      ".aim__first",
+      {
+        xPercent: -100,
+      },
+      "0.5"
+    )
+    .to(
+      ".aim__second",
+      {
+        xPercent: -100,
+      },
+      "<"
+    )
+    .from(".aim__text", {
+      autoAlpha: 0,
+      yPercent: 100,
+    })
+    .from(".experience__item", {
+      autoAlpha: 0,
+      stagger: 0.2,
+    })
+    .to(".experience__border", {
+      width: "100%",
+    });
+
+  const tlEducation = gsap.timeline({
+    repeat: -1,
+  });
+
+  tlEducation
+    .from(".education__item", {
+      duration: 1,
+      yPercent: 100,
+      autoAlpha: 0,
+      stagger: 2,
+    })
+    .to(
+      ".education__item",
+      {
+        duration: 1,
+        yPercent: -100,
+        stagger: 2,
+      },
+      2
+    );
+
+  gsap.set(".training__item:first-child", {
+    xPercent: 110,
+  });
+
+  gsap.set(".training__item:nth-child(2)", {
+    xPercent: 120,
+  });
+
+  gsap.set(".training__item:nth-child(3)", {
+    xPercent: 130,
+  });
+
+  gsap.set(".training__item:nth-child(4)", {
+    xPercent: 140,
+  });
+
+  gsap.set(".training__title", {
+    xPercent: 100,
+  });
+
+  const tlResults = gsap.timeline({
+    paused: true,
+  });
+
+  tlResults
+    .from(".results__item", {
+      xPercent: 100,
+      autoAlpha: 0,
+      stagger: 1,
+    })
+    .to(
+      ".results__item:not(:last-child)",
+      {
+        xPercent: -100,
+        stagger: 1,
+      },
+      1
+    );
+
+  const tlTraining = gsap.timeline({
+    paused: true,
+  });
+
+  tlTraining
+    .to(".results", {
+      xPercent: -100,
+    })
+    .to(
+      ".training__item",
+      {
+        xPercent: 0,
+      },
+      "<"
+    )
+    .to(
+      ".training__title",
+      {
+        xPercent: 0,
+      },
+      "<"
+    );
+
+  const tlDouble = gsap.timeline({});
+
+  tlDouble
+    .to(tlResults, {
+      duration: 0.8,
+      progress: 1,
+    })
+    .to(tlTraining, {
+      duration: 0.6,
+      progress: 1,
+      ease: "power3.in",
+    });
+
+  ScrollTrigger.create({
+    animation: tlDouble,
+    trigger: ".double-section",
+    start: "top top",
+    end: `bottom+=${tlDouble.duration() * 4000} bottom`,
+    pin: true,
+    scrub: 1,
+  });
+}
